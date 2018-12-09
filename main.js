00ad7a5895a01080a005448bbd09f904de237c19
nitoyon <nitoyon@gmail.com>
2018-12-10 04:22:59 +0900

    stat page

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var ctx = {
 	qid: "",
 	curQ: null,
 	q: {add1: [], add2: [], sub1: [], sub2: []},
-	done: {add1: 0, add2: 0, sub1: 0, sub2: 0}
+	done: {add1: 0, add2: 0, sub1: 0, sub2: 0},
+	result: []
 };
 
 (function() {
@@ -17,11 +18,19 @@ var ctx = {
 				var cur = ctx.q[ctx.qid];
 				cur.splice(cur.indexOf(ctx.curQ), 1);
 				ctx.done[ctx.qid]++;
+
+				var res = ctx.result[ctx.result.length - 1];
+				res.end = new Date();
+				res.ans.push({num: a, ok: true});
+
 				newQuiz();
 				if (cur.length > 0) {
 					$("#ok").show().delay(500).fadeOut(100);
 				}
 			} else {
+				var res = ctx.result[ctx.result.length - 1];
+				res.ans.push({num: a, ok: false});
+
 				btn.addClass('ng');
 				$("#ng").show().delay(500).fadeOut(100);
 				$("#q").css('background', 'red');
@@ -77,24 +86,54 @@ var ctx = {
 	};
 
 	function initMenu() {
-		$("div#select a").click(function() {
+		$("div#select section a").click(function() {
 			ctx.qid = this.id;
 			newQuiz();
 		});
-		$("div#qa header i").click(function() {
+		$("a#showstat").click(function() {
+			showStat();
+		});
+		$("header .back").click(function() {
 			showMenu();
 		});
 	}
 
 	function showMenu() {
 		$("#select").show();
-		$("#qa, #done").hide();
+		$("#qa, #stat, #done").hide();
 
 		$("#select a").each(function(i, el) {
 			var done = ctx.done[el.id];
 			if (done !== 0) {
 				$(this).find(".done").text(done + "もん");
 			}
+		});
+	}
+
+	function showStat() {
+		$("#select, #qa, #done").hide();
+		$("#stat").show();
+
+		var section = $("#stat section");
+		section.children().remove();
+		if (ctx.result.length === 0) {
+			$("<h2>").text("まだ 1 問も解いていません。").appendTo(section);
+			return;
+		}
+
+		var table = $("<table>").appendTo(section);
+		$.each(ctx.result, function(i, item) {
+			var tr = $("<tr>").appendTo(table);
+			$("<td>").addClass("q").text(item.q).appendTo(tr);
+			var ans = $("<td>").addClass("ans").appendTo(tr);
+			$.each(item.ans, function(j, a) {
+				$("<span>").addClass(a.ok ? "ok" : "ng").text(a.num).appendTo(ans);
+				$("<br>").appendTo(ans);
+			});
+			$("<td>").addClass("time")
+				.text(item.end ?
+					(Math.floor((item.end.getTime() - item.start.getTime()) / 100) / 10) + " 秒" : "----")
+				.appendTo(tr);
 		});
 	}
 
@@ -128,9 +167,16 @@ var ctx = {
 		resize();
 
 		ctx.curQ = cur[Math.floor(Math.random() * cur.length)];
-		$("#q span").text(ctx.curQ[0] +
+		var q = ctx.curQ[0] +
 			(ctx.qid.charAt(0) === 'a' ? " + " : " - ") +
-			ctx.curQ[1]);
+			ctx.curQ[1];
+		$("#q span").text(q);
+		ctx.result.push({
+			q: q,
+			start: new Date(),
+			end: null,
+			ans: [],
+		});
 	}
 
 	init();
