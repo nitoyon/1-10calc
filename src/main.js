51b09a4e5ae1a34033d3b469cb9451faf58b5397
nitoyon <nitoyon@gmail.com>
2019-01-14 01:46:05 +0900

    Move Solve component to vue file

--- src/main.js
+++ src/main.js
@@ -1,119 +1,10 @@
-Vue.component('app-solve', {
-  props: {
-    'buttons': {
-      type: Array,
-      default: function() {
-        return [
-          { label: 1, ng: false },
-          { label: 2, ng: false },
-          { label: 3, ng: false },
-          { label: 4, ng: false },
-          { label: 5, ng: false },
-          { label: 6, ng: false },
-          { label: 7, ng: false },
-          { label: 8, ng: false },
-          { label: 9, ng: false },
-          { label: 10, ng: false }
-        ];
-      }
-    },
-    'category': Object,
-    'histories': Array
-  },
-
-  mounted: function() {
-    this.newQuiz();
-  },
-
-  methods: {
-    initButtons: function() {
-      var over10 = (this.category.id === 'add2' ? 11 : 1);
-
-      for (var i = 0; i < this.buttons.length; i++) {
-        this.buttons[i].label = i + over10;
-        this.buttons[i].ng = false;
-      }
-    },
-
-    newQuiz: function() {
-      this.initButtons();
-
-      this.category.q = this.findQuestion();
-      if (this.category.q === null) {
-        return;
-      }
-
-      var q = this.category.q;
-      var t = q[0] + this.category.sign + q[1];
-      this.histories.push({
-        q: q,
-        text: t,
-        start: new Date(),
-        end: null,
-        ans: []
-      });
-    },
-
-    answer: function(btn) {
-      var clicked = btn.label;
-      var ans = this.category.q[2];
-
-      var self = this;
-      var h = this.histories[this.histories.length - 1];
-      if (ans !== clicked) {
-        h.ans.push({num: clicked, ok: false});
-        btn.ng = true;
-        this.category.isNG = true;
-        setTimeout(function() {
-          self.category.isNG = false;
-        }, 500);
-      } else {
-        h.end = new Date();
-        h.ans.push({num: ans, ok: true});
-        var all = this.category.questions;
-        all.splice(all.indexOf(this.category.q), 1);
-
-        this.category.isOK = true;
-        setTimeout(function() {
-          self.category.isOK = false;
-          self.category.done++;
-          self.newQuiz();
-        }, 500);
-      }
-    },
-
-    findQuestion: function() {
-      var all = this.category.questions;
-      if (all.length === 0) {
-        return null;
-      }
-
-      return all[Math.floor(Math.random() * all.length)];
-    }
-  },
-  template: `
-<div id="qa" class="page">
-  <header>
-    <i class="back fas fa-arrow-left" v-on:click="$emit('show-page', 'app-menu')"></i>
-    <div>{{category.done}} もんせいかい</div>
-  </header>
-  <div id="q" v-if="category.q" v-bind:class="{ok: category.isOK, ng: category.isNG}"><span>{{ category.q[0] }} {{ category.sign }} {{ category.q[1] }}</span> = <i class="fas fa-question"></i></div>
-  <div id="a" v-if="category.q">
-    <button v-for="b in buttons" v-on:click="answer(b)" v-bind:class="{ng: b.ng}">{{ b.label }}</button>
-  </div>
-  <transition name="fadeout"><div id="ok" v-if="category.isOK">🙆</div></transition>
-  <transition name="fadeout"><div id="ng" v-if="category.isNG">😢</div></transition>
-  <div id="done" v-if="!category.q">🎉</div>
-</div>
-`
-});
-
 import Menu from './components/Menu.vue'
+import Solve from './components/Solve.vue'
 import Stat from './components/Stat.vue'
 
 new Vue({
   el: '#app',
-  components: { Menu, Stat },
+  components: { Menu, Stat, Solve },
   template: `
     <div id="app">
       <Menu
@@ -122,12 +13,12 @@ new Vue({
         v-on:show-page="showPage($event)"
         v-on:set-category="setCategory($event)">
       </Menu>
-      <app-solve
+      <Solve
         v-if="currentPage == 'app-solve'"
         v-bind:category="selectedCategory"
         v-bind:histories="histories"
         v-on:show-page="showPage($event)">
-      </app-solve>
+      </Solve>
       <Stat
         v-if="currentPage == 'app-stat'"
         v-bind:histories="histories"
