e9478bf23a13128070957b41643651ba2357c7a0
nitoyon <nitoyon@gmail.com>
2019-01-25 22:52:45 +0900

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -eu
+cd `dirname $0`
+
+DST=../1-10calc-gh-pages
+SRC=./dist
+
+SHA=`git rev-parse HEAD`
+
+OUT=`git status --porcelain`
+if [ ! -z "${OUT}" ]; then
+	echo Directory is not clean
+	git status
+	exit 1
+fi
+
+if [ ! -d $DST ]; then
+	echo Creating $DST...
+	git worktree add $DST gh-pages
+fi
+
+# Build
+echo Building to $SRC
+#npm run build
+
+# Delete all files in $DST (except for .git)
+echo Deleting $DST...
+rm -rf $DST/*
+
+# Copy SRC to DST
+echo Copying $SRC to $DST...
+cp -r $SRC/* $DST/
+
+# commit
+echo Commiting...
+cd $DST
+git add -A .
+git commit -m "Deploy $SHA"
